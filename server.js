b930dfdd382787f4fbb6af0025cab2dfc56e3a48
“kara <bouncingpie@icloud.com>
2022-11-30 00:24:52 -0500

    added notes route and api

--- server.js
+++ server.js
@@ -2,30 +2,38 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const { clog } = require("./middleware/clog");
 
 // Set PORT variable
 const PORT = process.env.PORT || 3001;
-
-// Import Modules and files
-import express from "express";
-
+app.use(clog);
 // Set express static route
-app.use("/static", express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", api);
 // Set express functions
 
 // GET route for homepage
-app.get("/", (req, res) => {
-  res.send("GET request to the homepage");
+
+// sendFile will go here
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 // GET route for notes
 app.get("/notes", (req, res) => {
-  res.send("GET request to the homepage");
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 // Post APi route for notes
+app.post("/api/notes", (req, res) => {
+  console.log(123);
+  console.log(req);
+});
 
 // DELETE API route for notes
 
 // Set listening on PORT
+app.listen(PORT, () => console.log("server running"));
